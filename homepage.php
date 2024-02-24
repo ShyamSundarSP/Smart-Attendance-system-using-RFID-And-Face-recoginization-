59472f0d4408bd5a039b0052c3b398688541128a
Shyam Sundar S P <146634390+ShyamSundarSP@users.noreply.github.com>
2024-02-24 11:32:03 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,49 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="homestyle.css" type="text/css" />
+    <title></title>
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">GameChangers.School</a>
+        <nav class="nav-items">
+        </nav>
+    </header>
+    <main>
+        <div class="intro">
+            <br />
+            <br />
+            <br />
+            <h1>Students Specialized Attendance Monitory</h1>
+            <p>I am a student and I love to create future</p>
+            <a href="nothinglogin.html"><button>login as a student</button></a>
+            <br />
+            <br />
+            <a href="facultylogin.html"><button>login as a teacher</button></a>
+            <div>
+                <div class="about-me">
+                    <div class="about-me-text">
+                        <br />
+                        <br />
+                        <br />
+                        <h2>About GameChangers</h2>
+                        <br />
+                        <p><bold>GameChangers</bold> are the most wanted technological developers in a technical society.</p>
+                    </div>
+                </div>
+</main>
+    <footer class="footer">
+        <div class="bottom-links">
+            <div class="links">
+                <span>Social Links</span>
+                <a href="#"><img src="C:\Users\anton\source\repos\instagram.JPG"></a>
+                <a href="#"><img src="C:\Users\anton\source\repos\facebook.JPG"></a>
+                <a href="#"><img src="C:\Users\anton\source\repos\twitter.JPG"></a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ nothinglogin.php
@@ -0,0 +1,41 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Login Page</title>
+    <link rel="icon" href="icon1.png" type="image/x-icon" />
+    <link rel="stylesheet" href="loginstyle.css" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="login-div">
+        <div class="logo"><img src="login man2.png" height="150" width="150" alt="" /></div>
+        <div class="title"><strong>Login</strong></div>
+        <div class="sub-title">Attendance Monitor</div>
+
+        <div class="form">
+            <div class="username">
+                <input type="text" placeholder="Username or Email" />
+            </div>
+            <div class="password">
+                <input type="password" placeholder="Password" />
+            </div>
+            <div class="option">
+                <div class="remember-me">
+                    <input id="remember-me" type="checkbox" />
+                    <label for="remember-me">Remember-me?</label>
+                </div>
+                <div class="forget-password">
+                    <a href="#">Forget Password?</a>
+                </div>
+
+            </div>
+        </div>
+        <button class="signin-bn">SIGN IN</button>
+    </div>
+
+</body>
+</html>
